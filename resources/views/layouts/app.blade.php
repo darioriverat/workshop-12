29932fff06a22a49fda236d37c8d4a0fd56ecf58
luis felipe g <luis.garcia@placetopay.co>
2020-04-28 13:59:26 -0500

    changes in .env.production

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -101,7 +101,7 @@
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                 <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                    @lang('config.navbar.forms')
+                                    @lang('config.navbar.logout')
                                 </a>
 
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST"
